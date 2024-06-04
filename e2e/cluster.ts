5b0de9cbf97d60839e98242a4546f30c12959f59
hgpark <hanguyl91@gmail.com>
2024-06-05 08:01:46 +0900

    fix: invalid e2e target

--- e2e/cluster.ts
+++ e2e/cluster.ts
@@ -8,10 +8,17 @@ const clusterRunner = new ClusterRunner({
 });
 
 await clusterRunner.start({
-  command: ['bun', 'src/index.ts'],
+  command: ['bun', 'e2e/index.ts'],
   reloadSignal: 'SIGHUP',
   updateEnv: false,
 });
+await Bun.sleep(1000);
+const process = clusterRunner.subprocessList[0]
+process[1].kill()
+await process[1].exited
+console.log(`process: ${process[1].pid} pre-killed`)
+await Bun.sleep(1000);
 
-await Bun.sleep(3000);
 await clusterRunner.reload();
+await Bun.sleep(1000);
+await clusterRunner.terminate()
